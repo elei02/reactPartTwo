71024e52caa8ca7f47e8c7a7db9ca22b7cdfd884
eric lei <elei02@calpoly.edu>
2021-04-02 09:06:58 -0700

    ptII5

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,27 +1,16 @@
 import React, {useState} from 'react';
 import Table from './Table'
+import Form from './Form';
+
 //const characters is called a prop
 function MyApp() {
-  const [characters, setCharacters] = useState([
-      {
-        name: 'Charlie',
-        job: 'Janitor',
-         // the rest of the data
-      },
-      {
-        name: 'Dee',
-        job: 'Aspiring Actress',
-      },
-      {
-        name: 'Dennis',
-        job: 'Bartender',
-      },
-    ])
-    return (
-      <div className="container">
-        <Table characterData={characters} removeCharacter={removeOneCharacter} />
-      </div>
-    );  
+  const [characters, setCharacters] = useState([])
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form />
+    </div>
+  );  
 
 function removeOneCharacter (index) {
   const updated = characters.filter((character, i) => {
