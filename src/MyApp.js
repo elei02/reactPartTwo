d11770b27d1ae48524d912397ef106f4309dcd3e
eric lei <elei02@calpoly.edu>
2021-04-10 18:17:10 -0700

    Done with 3 and halfway through 4

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,9 +1,17 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table'
 import Form from './Form';
+import axios from 'axios';
+
 
 //const characters is called a prop
 function MyApp() {
+  useEffect(() => {
+    fetchAll().then( result => {
+       if (result)
+          setCharacters(result);
+    });
+  }, [] );
   const [characters, setCharacters] = useState([])
   return (
     <div className="container">
@@ -19,8 +27,47 @@ function removeOneCharacter (index) {
     setCharacters(updated);
   }
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
+  function updateList(person) { 
+    makePostCall(person).then( result => {
+    if (result)
+       setCharacters([...characters, person] );
+    });
+ }
+}
+
+async function fetchAll(){
+  try {
+     const response = await axios.get('http://localhost:5000/users');
+     return response.data.users_list;     
+  }
+  catch (error){
+     //We're not handling errors. Just logging into the console.
+     console.log(error); 
+     return false;         
+  }
+}
+
+async function deleteCall(person){
+  try {
+     var lin = 'http://localhost:5000/users/' + person['id']
+     const response = await axios.delete(lin);
+     return response.data.users_list;     
+  }
+  catch (error){
+     //We're not handling errors. Just logging into the console.
+     console.log(error); 
+     return false;         
+  }
+}
+
+async function makePostCall(person){
+  try {
+     const response = await axios.post('http://localhost:5000/users', person);
+     return response;
+  }
+  catch (error) {
+     console.log(error);
+     return false;
   }
 }
 
